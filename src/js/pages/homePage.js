a8ed7a11db3c50f55f8d1fbd3d19081a25d3afca
namesaregreat <76820450+namesaregreat@users.noreply.github.com>
2021-10-19 23:31:37 -0600

    test 9

--- src/js/pages/homePage.js
+++ src/js/pages/homePage.js
@@ -1,13 +1,13 @@
 import { logo } from "../components/logo";
 import header from "../components/header";
-import button from "../components/link";
 import makeElement from "../utils/makeElement";
+import link from "../components/link";
 
 const homePage = function() {
     const page = document.createElement('header')
     page.append(makeElement(logo))
     page.append(makeElement(header('A Simple Way to Manage your Work','ui-small-header')))
-    page.append(makeElement(button('To Do List')))
+    page.append(makeElement(link('To Do List')))
 
     return page
 }
